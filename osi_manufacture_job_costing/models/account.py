68f47b641c80987213f550eb164063ebe2ef379b
Balaji Kannan <bkannan@ursainfosystems.com>
2019-08-30 13:32:59 -0700

    included api in import

--- osi_manufacture_job_costing/models/account.py
+++ osi_manufacture_job_costing/models/account.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2019 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import models, api, fields
 
 class AnalyticAccountLine(models.Model):
     _inherit = 'account.analytic.line'
