93642ed49949593ec82c8ed4cad05c6b4c31e303
Sandip Mangukiya <smangukiya@ursainfosystems.com>
2019-09-10 13:19:34 -0700

    [IMP]JOB COSTING: improve module to avoid WIP TO FG and FG TO COGS JE, and support direct WIP TO COGS

--- osi_manufacture_job_costing/models/stock.py
+++ osi_manufacture_job_costing/models/stock.py
@@ -113,52 +113,52 @@ class StockMove(models.Model):
         else:
             super()._run_valuation(quantity=quantity)
 
-    def _account_entry_move(self):
-        """ Accounting Valuation Entries """
-        self.ensure_one()
-        if self.product_id.type == 'product':
-            # no stock valuation for consumable products
-            return super(StockMove, self)._account_entry_move()
-        if self.product_id.type == 'service':
-            # no stock valuation for consumable products
-            return False
-        if self.restrict_partner_id:
-            # if the move isn't owned by the company, we don't make any valuation
-            return False
+    # def _account_entry_move(self):
+    #     """ Accounting Valuation Entries """
+    #     self.ensure_one()
+    #     if self.product_id.type == 'product':
+    #         # no stock valuation for consumable products
+    #         return super(StockMove, self)._account_entry_move()
+    #     if self.product_id.type == 'service':
+    #         # no stock valuation for consumable products
+    #         return False
+    #     if self.restrict_partner_id:
+    #         # if the move isn't owned by the company, we don't make any valuation
+    #         return False
 
-        location_from = self.location_id
-        location_to = self.location_dest_id
-        company_from = self._is_out() and self.mapped('move_line_ids.location_id.company_id') or False
-        company_to = self._is_in() and self.mapped('move_line_ids.location_dest_id.company_id') or False
+    #     location_from = self.location_id
+    #     location_to = self.location_dest_id
+    #     company_from = self._is_out() and self.mapped('move_line_ids.location_id.company_id') or False
+    #     company_to = self._is_in() and self.mapped('move_line_ids.location_dest_id.company_id') or False
 
-        # Create Journal Entry for products arriving in the company; in case of routes making the link between several
-        # warehouse of the same company, the transit location belongs to this company, so we don't need to create accounting entries
-        if self._is_in():
-            journal_id, acc_src, acc_dest, acc_valuation = self._get_accounting_data_for_valuation()
-            if location_from and location_from.usage == 'customer':  # goods returned from customer
-                self.with_context(force_company=company_to.id)._create_account_move_line(acc_dest, acc_valuation, journal_id)
-            else:
-                self.with_context(force_company=company_to.id)._create_account_move_line(acc_src, acc_valuation, journal_id)
+    #     # Create Journal Entry for products arriving in the company; in case of routes making the link between several
+    #     # warehouse of the same company, the transit location belongs to this company, so we don't need to create accounting entries
+    #     if self._is_in():
+    #         journal_id, acc_src, acc_dest, acc_valuation = self._get_accounting_data_for_valuation()
+    #         if location_from and location_from.usage == 'customer':  # goods returned from customer
+    #             self.with_context(force_company=company_to.id)._create_account_move_line(acc_dest, acc_valuation, journal_id)
+    #         else:
+    #             self.with_context(force_company=company_to.id)._create_account_move_line(acc_src, acc_valuation, journal_id)
 
-        # Create Journal Entry for products leaving the company
-        if self._is_out():
-            journal_id, acc_src, acc_dest, acc_valuation = self._get_accounting_data_for_valuation()
-            if location_to and location_to.usage == 'supplier':  # goods returned to supplier
-                self.with_context(force_company=company_from.id)._create_account_move_line(acc_valuation, acc_src, journal_id)
-            else:
-                self.with_context(force_company=company_from.id)._create_account_move_line(acc_valuation, acc_dest, journal_id)
+    #     # Create Journal Entry for products leaving the company
+    #     if self._is_out():
+    #         journal_id, acc_src, acc_dest, acc_valuation = self._get_accounting_data_for_valuation()
+    #         if location_to and location_to.usage == 'supplier':  # goods returned to supplier
+    #             self.with_context(force_company=company_from.id)._create_account_move_line(acc_valuation, acc_src, journal_id)
+    #         else:
+    #             self.with_context(force_company=company_from.id)._create_account_move_line(acc_valuation, acc_dest, journal_id)
 
-        if self.company_id.anglo_saxon_accounting:
-            # Creates an account entry from stock_input to stock_output on a dropship move. https://github.com/odoo/odoo/issues/12687
-            journal_id, acc_src, acc_dest, acc_valuation = self._get_accounting_data_for_valuation()
-            if self._is_dropshipped():
-                self.with_context(force_company=self.company_id.id)._create_account_move_line(acc_src, acc_dest, journal_id)
-            elif self._is_dropshipped_returned():
-                self.with_context(force_company=self.company_id.id)._create_account_move_line(acc_dest, acc_src, journal_id)
+    #     if self.company_id.anglo_saxon_accounting:
+    #         # Creates an account entry from stock_input to stock_output on a dropship move. https://github.com/odoo/odoo/issues/12687
+    #         journal_id, acc_src, acc_dest, acc_valuation = self._get_accounting_data_for_valuation()
+    #         if self._is_dropshipped():
+    #             self.with_context(force_company=self.company_id.id)._create_account_move_line(acc_src, acc_dest, journal_id)
+    #         elif self._is_dropshipped_returned():
+    #             self.with_context(force_company=self.company_id.id)._create_account_move_line(acc_dest, acc_src, journal_id)
 
-        if self.company_id.anglo_saxon_accounting:
-            #eventually reconcile together the invoice and valuation accounting entries on the stock interim accounts
-            self._get_related_invoices()._anglo_saxon_reconcile_valuation(product=self.product_id)
+    #     if self.company_id.anglo_saxon_accounting:
+    #         #eventually reconcile together the invoice and valuation accounting entries on the stock interim accounts
+    #         self._get_related_invoices()._anglo_saxon_reconcile_valuation(product=self.product_id)
 
 class StockLocation(models.Model):
     _inherit = 'stock.location'
